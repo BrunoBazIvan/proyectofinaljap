f08940b4f4072f05119629ed2bcfda884ec6c92c
BrunoBazIvan <131315692+BrunoBazIvan@users.noreply.github.com>
2023-10-17 10:33:54 -0300

    carrito costos

--- js/carrito.js
+++ js/carrito.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
 // Ejecutar una función después de 2000 milisegundos (2 segundos)
-setTimeout(function() {
+// setTimeout(function() {
 
-    const carrito = [];
-   // Obtener el elemento con el id 'btnCarrito'
-    const btnCarrito = document.getElementById('btnCarrito');
+//     const carrito = [];
+//    // Obtener el elemento con el id 'btnCarrito'
+//     const btnCarrito = document.getElementById('btnCarrito');
 
-    // Agregar un evento de clic al elemento
-    btnCarrito.addEventListener('click', () => {
-        console.log('hola');
-    });
+//     // Agregar un evento de clic al elemento
+//     btnCarrito.addEventListener('click', () => {
+//         console.log('hola');
+//     });
 
-}, 1000);
+// }, 1000);
 
 
 
